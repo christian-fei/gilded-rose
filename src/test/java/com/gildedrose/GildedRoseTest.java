9caf5d4e8583eb90f7076278daf962259a24a6bb
Christian Fei <crifei93@gmail.com>
2016-01-22 16:17:21 +0100

    extracted utility method for creating items in tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,7 @@ public class GildedRoseTest {
 	public void
 	increases_quality_by_3_for_concert_ticket_when_sellin_less_than_6_and_quality_less_than_50() throws Exception {
         int initialQuality = 30;
-		Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, initialQuality) };
+		Item[] items = createItemsWith("Backstage passes to a TAFKAL80ETC concert", 5, initialQuality);
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(initialQuality + 3, app.items[0].quality);    	
@@ -21,7 +21,7 @@ public class GildedRoseTest {
 	public void
 	increases_quality_by_2_for_concert_ticket_when_sellin_less_than_11_and_quality_less_than_50() throws Exception {
         int initialQuality = 30;
-		Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, initialQuality) };
+		Item[] items = createItemsWith("Backstage passes to a TAFKAL80ETC concert", 10, initialQuality);
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(initialQuality + 2, app.items[0].quality);    	
@@ -31,7 +31,7 @@ public class GildedRoseTest {
 	public void
 	increases_quality_for_aged_brie_while_quality_is_less_than_50() throws Exception {
     	for(int initialQuality=0; initialQuality <= 50; initialQuality++) {
-    		Item[] items = new Item[] { new Item("Aged Brie", 10, initialQuality) };
+    		Item[] items = createItemsWith("Aged Brie", 10, initialQuality);
     		GildedRose app = new GildedRose(items);
     		app.updateQuality();
     		if( initialQuality < 50 ){
@@ -40,6 +40,10 @@ public class GildedRoseTest {
     			assertEquals(initialQuality, app.items[0].quality);    	
     		}
     	}
+	}
+
+	private Item[] createItemsWith(String itemName, int itemSellin, int initialQuality) {
+		return new Item[] { new Item(itemName, itemSellin, initialQuality) };
 	}
 
 }
