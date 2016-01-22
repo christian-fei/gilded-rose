d6a75347484365114ff14b4e00567ec4c7394260
Christian Fei <crifei93@gmail.com>
2016-01-22 16:20:33 +0100

    extracted factory for gilded rose app with item

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,8 +11,7 @@ public class GildedRoseTest {
 	public void
 	increases_quality_by_3_for_concert_ticket_when_sellin_less_than_6_and_quality_less_than_50() throws Exception {
         int initialQuality = 30;
-		Item[] items = createItemsWith("Backstage passes to a TAFKAL80ETC concert", 5, initialQuality);
-        GildedRose app = new GildedRose(items);
+		GildedRose app = createAppWithItem("Backstage passes to a TAFKAL80ETC concert", 5, initialQuality);
         app.updateQuality();
         assertEquals(initialQuality + 3, app.items[0].quality);    	
 	}
@@ -21,18 +20,17 @@ public class GildedRoseTest {
 	public void
 	increases_quality_by_2_for_concert_ticket_when_sellin_less_than_11_and_quality_less_than_50() throws Exception {
         int initialQuality = 30;
-		Item[] items = createItemsWith("Backstage passes to a TAFKAL80ETC concert", 10, initialQuality);
-        GildedRose app = new GildedRose(items);
+		GildedRose app = createAppWithItem("Backstage passes to a TAFKAL80ETC concert", 10, initialQuality);
         app.updateQuality();
         assertEquals(initialQuality + 2, app.items[0].quality);    	
 	}
+
     
     @Test
 	public void
 	increases_quality_for_aged_brie_while_quality_is_less_than_50() throws Exception {
     	for(int initialQuality=0; initialQuality <= 50; initialQuality++) {
-    		Item[] items = createItemsWith("Aged Brie", 10, initialQuality);
-    		GildedRose app = new GildedRose(items);
+    		GildedRose app = createAppWithItem("Aged Brie", 10, initialQuality);
     		app.updateQuality();
     		if( initialQuality < 50 ){
     			assertEquals(initialQuality + 1, app.items[0].quality);    	
@@ -46,4 +44,9 @@ public class GildedRoseTest {
 		return new Item[] { new Item(itemName, itemSellin, initialQuality) };
 	}
 
+	private GildedRose createAppWithItem(String itemName, int itemSellin, int itemQuality) {
+		Item[] items = createItemsWith(itemName, itemSellin, itemQuality);
+		GildedRose app = new GildedRose(items);
+		return app;
+	}
 }
