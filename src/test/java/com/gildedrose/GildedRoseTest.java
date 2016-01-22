a1161d83872bfacd3f23fefb124d4dd078c6213d
Christian Fei <crifei93@gmail.com>
2016-01-22 15:41:50 +0100

    added a test for increasing quality for concert ticket

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,18 +1,19 @@
 package com.gildedrose;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 
 public class GildedRoseTest {
-
-    @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+	
+	@Test
+	public void test_nothing() throws Exception {
+		int initialQuality = 30;
+		Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, initialQuality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
-    }
+        assertEquals(app.items[0].name, "Backstage passes to a TAFKAL80ETC concert");
+	}
 
 }
